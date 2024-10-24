20dc4400214bafecce0b9d141b85135727799af2
sungyun <tjddbs1201@naver.com>
2024-10-24 02:47:46 +0000

    task

---
+++ midterm/storeupdate.c
@@ -0,0 +1,69 @@
+   #include <stdio.h>
+  #include <stdlib.h>
+   #include <unistd.h>
+   #include <fcntl.h>
+  #include "db.dat"
+  int main(int argc, char *argv[])
+   {
+     int fd, num;
+      char c;
+     struct db record;
+     if (argc < 2) {
+        fprintf(stderr, "How to use : %s file\n", argv[0]);
+        exit(1);
+     }
+     if ((fd = open(argv[1], O_RDONLY)) == -1) {
+        perror(argv[1]);
+        exit(2);
+     }
+     do {
+ 
+        if (scanf("%d", &num == 1))
+        {
+        	
+			printf("--Goods update--\n");
+			printf("(n): sell one goods ]")
+			if(num == 1)
+			{
+				if (record.stock == 1){
+					stock = stock - 1;
+        			while(fread(&record, sizeof(record), l,fp) > 0)
+           
+            
+					printf("%d      %s          %s          %d              %d", record.id,record.name, record.category, record.expired_date, record.stock);
+ 				}
+				else
+					printf("'SpecailTea' has been sold ooout.. (There is no stock\n");
+				    while(fread(&record, sizeof(record), l,fp) > 0)
+
+
+                    printf("%d      %s          %s          %d              %d", record.id,record.name, record.category, record.expired_date, record.stock);
+			}
+	
+ 
+        }
+        if scanf("%d", &num == 1)
+        {
+         if(record.stock == 0 or record.expired_date <=2023)
+          {
+              if ((read(fd, (char *) &record, sizeof(record)) > 0) && (record.num != 0))
+                      printf("0: list of all goods, 1: list of available goods\n");
+                      printf("id      name        category    expired date    stock\n");
+                      printf("%d      %s          %s          %d              %d", record.id,record.name, record.category, record.expired_date, record.stock);
+          }
+        }
+ 
+ 
+ 
+ 
+ 
+   }
+    close(fd);
+    exit(0);
+  }
+ 
+
+
+
+
+
